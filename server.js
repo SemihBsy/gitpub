559ff36a461c8c4f274499552441a075b9d84034
SemihB <semihr5171@gmail.com>
2022-11-05 23:16:22 -0400

    drinks

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express();
 
 const PORT = 3000
 
-app.get('/', (request, response) => {
-    response.send('Welcome to the Gitpub App!');
+const drinks = require('./models/drinks')
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Gitpub App!');
 });
+
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
+})
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
