755afdc56f9e4a0590f8fe1e12227212ef49ade7
SemihB <semihr5171@gmail.com>
2022-11-12 15:47:54 -0500

    Ready to Serve

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-
-const PORT = 3000
-
-const drinks = require('./models/drinks')
+const PORT = process.env.PORT;
+const drinks = require("./models/drinks");
+const food = require("./models/food");
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Gitpub App!');
+    res.send('Welcome to the Gitpub App!')
 });
 
-app.get('/drinks', (req, res) => {
-    res.render('drinks_index.ejs', {
-        drinks: [...drinks]
+app.get('/menu', (req, res) => {
+    res.render("menu_index.ejs", {
+        drinks: drinks,
+        food: food
     })
 });
 
 app.get('/drinks/:id', (req, res) => {
     res.render('drinks_show.ejs', {
-        drink: drinks[req.params.id]
+        drink: drinks[req.params.id],
+        index: req.params.id
     })
-})
+});
+
+app.get('/food/:id', (req, res) => {
+    res.render('food_show.ejs', {
+        food: food[req.params.id],
+        index: req.params.id
+    })
+});    
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
